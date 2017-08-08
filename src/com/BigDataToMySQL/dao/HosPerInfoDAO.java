3022865d927a6a2d0d22e3cd04f93b09602b4f44
ZTom <yizhaopu2008@163.com>
2017-08-08 17:39:23 +0800

    add

---
+++ src/com/BigDataToMySQL/dao/HosPerInfoDAO.java
@@ -0,0 +1,67 @@
+package com.BigDataToMySQL.dao;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+
+import com.BigDataToMySQL.entity.HosPerInfo;
+
+public class HosPerInfoDAO extends BaseDAO{
+	
+	private String tableName = "hos_per_info";
+	private String id_column = "id";
+	private String visit_no_column = "visit_no";
+	private String ill_no_column = "ill_no";
+	private String ill_name_column = "ill_name";
+	private String visit_time_column = "visit_time";
+	private String patient_no_column = "patient_no";
+	private String patient_area_no_column = "patient_area_no";
+	private String patient_area_addr_column = "patient_area_addr";
+	private String patient_gender_column = "patient_gender";
+	private String patient_age_column = "patient_age";
+		
+	public HosPerInfoDAO(){
+		super();
+	}
+	public void insertData(List<HosPerInfo> list){
+		System.out.println("Start inserting data ......");
+		System.out.println("Insert size: " + list.size());
+		for(HosPerInfo hpi : list){
+			String sql = "INSERT INTO `" + tableName + "`" +
+					"(`" + id_column + "`,"  + 
+					"`"  + visit_no_column + "`," + 
+					"`"  + ill_no_column + "`," + 
+					"`"  + ill_name_column + "`," + 
+					"`"  + patient_no_column + "`," +
+					"`"  + patient_area_no_column + "`," +
+					"`"  + patient_area_addr_column + "`," +
+					"`"  + patient_gender_column + "`," +
+					"`"  + patient_age_column + "`)" + 
+					"VALUES (" + 
+						"null" + ",'" + 
+						hpi.getVisitNo() + "','" +
+						hpi.getIllNo() + "','" +
+						hpi.getIllName() + "','" +
+						hpi.getVisitTime() + "','" +
+						hpi.getPatientNo() + "','" +
+						hpi.getPatientAreaNo() + "','" +
+						hpi.getPatientAddr() + "','" +
+						hpi.getPatientGender() + "','" +
+						hpi.getPatientAge() +
+					"')";
+			System.out.println(sql);
+			try {
+				Statement stmt = conn.createStatement();
+				boolean success = stmt.execute(sql);
+				System.out.println(success);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+	}
+}
--- src/com/BigDataToMySQL/test/test.java
+++ src/com/BigDataToMySQL/test/test.java
@@ -10,31 +10,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.BigDataToMySQL.dao.DocInfoDAO;
+import com.BigDataToMySQL.dao.HosPerInfoDAO;
 import com.BigDataToMySQL.entity.DocInfo;
+import com.BigDataToMySQL.entity.HosPerInfo;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DocInfoDAO did = new DocInfoDAO();
+//		DocInfoDAO did = new DocInfoDAO();
+		HosPerInfoDAO hpid = new HosPerInfoDAO();
 //		List<DocInfo> list = loadDocInfo();
-		loadDocInfo();
+		List<HosPerInfo> list = null ;//loadHosPerInfo();		
+//		loadDocInfo();
+		loadHosPerInfo();
+//		hpid.insertData(list);
 //		did.insertData(list);
 //		did.selectData();
 	}
 	
-	public static void loadDocInfo(){
-		File doc_info = new File("C://Users/vm/Desktop/documents/111.txt");
-		if(doc_info.exists() && doc_info.isFile()){
+//	public static void loadDocInfo(){
+//		File doc_info = new File("C://Users/Administrator/Desktop/code/hos_per_info_reduced.txt");
+//		if(doc_info.exists() && doc_info.isFile()){
+//			try {
+//				InputStreamReader read = new InputStreamReader(new FileInputStream(doc_info), "utf8");
+//				BufferedReader bufferedReader = new BufferedReader(read);
+//				String dataStr = null;   
+//				dataStr = bufferedReader.readLine().trim();
+//		        String[] datas = dataStr.split(",");
+//		        for(String d : datas){
+//		        	System.out.print(d + "-");
+//		        }
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			
+//			
+//		}
+//	}
+	
+	public static void loadHosPerInfo(){
+		File hos_Per_info = new File("C://Users/Administrator/Desktop/code/hos_per_info_reduced.txt");
+		InputStreamReader read = null;
+		BufferedReader bufferedReader = null;
+		if(hos_Per_info.exists() && hos_Per_info.isFile()){
 			try {
-				InputStreamReader read = new InputStreamReader(new FileInputStream(doc_info), "utf8");
-				BufferedReader bufferedReader = new BufferedReader(read);
-				String dataStr = null;   
-				dataStr = bufferedReader.readLine().trim();
-		        String[] datas = dataStr.split(",");
-		        for(String d : datas){
-		        	System.out.print(d + "-");
-		        }
+				read = new InputStreamReader(new FileInputStream(hos_Per_info), "utf8");
+				bufferedReader = new BufferedReader(read);
+				String dataStr = null;  
+				while(bufferedReader.readLine() != null){
+					dataStr = bufferedReader.readLine().trim();
+					String[] datas = dataStr.split(",");
+					for(String d : datas){
+						System.out.print(d + "-");
+					}
+				}
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -43,5 +74,4 @@ public class test {
 			
 		}
 	}
-
 }
